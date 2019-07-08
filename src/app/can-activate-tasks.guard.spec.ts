5870a5b9c65116aead7469df1820f20880ad5a40
Snezhana Novitskaya <s.novitskaya@andersenlab.com>
2019-07-08 17:32:17 +0300

    style

--- src/app/can-activate-tasks.guard.spec.ts
+++ src/app/can-activate-tasks.guard.spec.ts
@@ -3,13 +3,13 @@ import { TestBed, inject } from '@angular/core/testing';
 import { CanActivateTasksGuard } from './can-activate-tasks.guard';
 
 describe('CanActivateTasksGuard', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [CanActivateTasksGuard]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [CanActivateTasksGuard],
+        });
     });
-  });
 
-  it('should ...', inject([CanActivateTasksGuard], (guard: CanActivateTasksGuard) => {
-    expect(guard).toBeTruthy();
-  }));
+    it('should ...', inject([CanActivateTasksGuard], (guard: CanActivateTasksGuard) => {
+        expect(guard).toBeTruthy();
+    }));
 });
